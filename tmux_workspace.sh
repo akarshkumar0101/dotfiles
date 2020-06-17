7a279d940ce041edf3298cb3a33792db2d5697ad
Akarsh Kumar <akarshkumar0101@gmail.com>
2020-06-16 20:36:56 -0500

    change variable name typo in tmux script

--- tmux_workspace.sh
+++ tmux_workspace.sh
@@ -13,21 +13,21 @@ if [ $? != 0 ]
     # to send a command to a pane:
     # tmux send-keys -t 'Shell' "zsh" C-m 'clear' C-m
 
-    tmux rename-window -t 0 'main'
+    tmux rename-window -t $SESSIONNAME:0 'main'
     tmux split-window -h -t $SESSIONNAME:0
     tmux split-window -v -t $SESSIONNAME:0
     tmux select-pane -t 0
     tmux split-window -v -t $SESSIONNAME:0
 
     
-    tmux new-window -t $SESSION:1 -n 'code'
+    tmux new-window -t $SESSIONNAME:1 -n 'code'
     # tmux send-keys -t 'Hugo Server' 'hugo serve -D -F' C-m # Switch to bind script?
     tmux split-window -h -t $SESSIONNAME:1
     tmux split-window -v -t $SESSIONNAME:1
     tmux select-pane -t 0
     tmux split-window -v -t $SESSIONNAME:1
 
-    tmux new-window -t $SESSION:2 -n 'data'
+    tmux new-window -t $SESSIONNAME:2 -n 'data'
     # tmux send-keys -t 'Writing' "nvim" C-m
     tmux split-window -h -t $SESSIONNAME:2
     tmux split-window -v -t $SESSIONNAME:2
@@ -35,7 +35,7 @@ if [ $? != 0 ]
     tmux split-window -v -t $SESSIONNAME:2
 
     # Setup an additional shell
-    tmux new-window -t $SESSION:3 -n 'process'
+    tmux new-window -t $SESSIONNAME:3 -n 'process'
     tmux split-window -h -t $SESSIONNAME:3
     tmux split-window -v -t $SESSIONNAME:3
     tmux select-pane -t 0
@@ -47,4 +47,5 @@ if [ $? != 0 ]
 fi
 
 # Attach Session, on the Main window
-tmux attach-session -t $SESSION:0
+tmux attach-session -t $SESSIONNAME:0
+
